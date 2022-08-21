6dcf83f0e40c6f81d568a0f7bf50505dfab093a5
kunalsingh2904 <kkunal654@gmail.com>
2022-08-21 11:35:39 +0530

    added admin page

--- master/api/admin.py
+++ master/api/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import VideoDetail
+
+admin.site.site_header = "YouTube Video DashBoard"
+
+class VideoDetailManager(admin.ModelAdmin):
+    list_display = (
+        'video_id',
+        'title',
+        'channel_name',
+        'published_datetime',
+        'thumbnail_url'
+    )
+    search_fields = ['title', 'channel_name']
+    list_filter = ['channel_name']
+
+
+admin.site.register(VideoDetail, VideoDetailManager)
