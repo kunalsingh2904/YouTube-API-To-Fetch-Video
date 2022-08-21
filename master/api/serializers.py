87c60bd8044f1b8ad381318cbea1ca19b2779cc2
kunalsingh2904 <kkunal654@gmail.com>
2022-08-21 19:24:25 +0530

    Added ListView with query

--- master/api/serializers.py
+++ master/api/serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 
 from .models import VideoDetail
 
-class YoutubeSerializer(serializers.ModelSerializer):
+class VideoDetailSerializer(serializers.ModelSerializer):
     class Meta:
         model = VideoDetail
         fields = '__all__'
--- master/api/views.py
+++ master/api/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
 
+from rest_framework.generics import ListAPIView
+from django.db.models import Q
+
+from .pagination import CustomPagination
+from .models import VideoDetail
+from .serializers import VideoDetailSerializer
 # Create your views here.
+
+class VideoDetailListAPIView(ListAPIView):
+    serializer_class = VideoDetailSerializer
+    model = VideoDetail
+    pagination_class = CustomPagination
+
+    def get_queryset(self):
+        queryset = self.model.objects.all()
+        query = self.request.query_params.get('query')
+        if query is not None:
+            # Add other serach field if required
+            queryset = queryset.filter(Q(title__icontains=query) | Q(description__icontains=query))
+        return queryset.order_by('-published_datetime')
